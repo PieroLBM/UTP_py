148c291cf59bad3e9369a9f4e73c36a40d8b39f4
Piero <ipgoov@gmail.com>
2025-09-01 01:07:34 -0500

    cambio2

--- prueba2.py
+++ prueba2.py
@@ -1,2 +1,9 @@
 valor_txt = input("Ingrese un valor de tempertura: ")
-print(f"El valor ingresado es: {valor_txt} °C")
+#print(f"El valor ingresado es: {valor_txt} °C")
+t=float(valor_txt)
+if t >= 30:
+    print("Alerta de temperatura alta")
+elif t < 0:
+    print("Alerta de temperatura baja")
+else:
+    print("Temperatura normal")
