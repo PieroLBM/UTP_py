aaebab7f54424dfca74a5a022911edf37999736d
Piero <ipgoov@gmail.com>
2025-09-01 00:43:57 -0500

    agregrando

---
+++ Semana 2.py
@@ -0,0 +1,7 @@
+NOMBRE = "Piero"
+edad = 20
+voltaje = 3.7
+activo = True
+# f-strings (Formato para cadenas de caracteres, letras)
+print(f"Hola, {NOMBRE}. edad: {edad}")
+print(f"Voltaje: {voltaje:.2f} V | Activo: {activo}")
